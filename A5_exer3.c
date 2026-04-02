c40071b7b00876f25bdb634c226839b054fec560
oliver-oliveira <oliver.oliveira@aluno.ifsp.edu.br>
2026-04-01 22:09:16 -0300

    Add files via upload

---
+++ A5_exer3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  float n1,n2, potencia;
+  
+    printf("X: ");
+    scanf("%f", &n1);
+    
+    printf("Y: ");
+    scanf("%f", &n2);
+    
+    potencia = pow(X^Y);
+    printf("Potencia de %.2f = %.2f", x, n2, potencia);
+
+    return 0;
+}
